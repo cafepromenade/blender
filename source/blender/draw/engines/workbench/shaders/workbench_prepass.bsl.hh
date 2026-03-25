c6bc8c91ca966dfb5fab79f95dadc9e6bdbd9480
Clément Foucault <foucault.clem@gmail.com>
2026-03-25 13:41:13 +0100

    Fix #155865: Metal: Workbench performance regression
    This was caused by clip distances not
    being optimized away anymore.
    
    This PR introduce a similar check in the MSL
    shader generator as there used to be
    before 67a03ea03e4362a465decb70c9786e0bb72b2a83.
    
    Annoyingly, this check need to now be tailored to workbench
    as defines and builtins cannot be removed using BSL
    `condition` attribute.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/156274

--- source/blender/draw/engines/workbench/shaders/workbench_prepass.bsl.hh
+++ source/blender/draw/engines/workbench/shaders/workbench_prepass.bsl.hh
@@ -120,6 +120,8 @@ struct Mesh {
   [[legacy_info]] ShaderCreateInfo draw_modelmat_with_custom_id;
   [[legacy_info]] ShaderCreateInfo drw_clipped;
 
+  /** WORKAROUND: This exact compilation constant is checked in Metal backend to enable clip
+   * distances. */
   [[compilation_constant]] const bool use_clipping;
 };
 
@@ -132,7 +134,7 @@ struct Mesh {
   float3 world_pos = drw_point_object_to_world(v_in.pos);
   out_position = drw_point_world_to_homogenous(world_pos);
 
-  if (mesh.use_clipping) {
+  if (mesh.use_clipping) [[static_branch]] {
     view_clipping_distances(world_pos);
   }
 
@@ -157,6 +159,8 @@ struct Curves {
   [[legacy_info]] ShaderCreateInfo draw_curves_infos;
   [[legacy_info]] ShaderCreateInfo drw_clipped;
 
+  /** WORKAROUND: This exact compilation constant is checked in Metal backend to enable clip
+   * distances. */
   [[compilation_constant]] const bool use_clipping;
 
   [[sampler(WB_CURVES_COLOR_SLOT) /*, frequency(batch)*/]] samplerBuffer ac;
@@ -193,7 +197,7 @@ struct Curves {
     nor = hair_random_normal(pt.curve_T, pt.curve_B, pt.curve_N, hair_rand);
   }
 
-  if (curves.use_clipping) {
+  if (curves.use_clipping) [[static_branch]] {
     view_clipping_distances(world_pos);
   }
 
@@ -227,6 +231,8 @@ struct PointCloud {
   [[legacy_info]] ShaderCreateInfo draw_pointcloud;
   [[legacy_info]] ShaderCreateInfo drw_clipped;
 
+  /** WORKAROUND: This exact compilation constant is checked in Metal backend to enable clip
+   * distances. */
   [[compilation_constant]] const bool use_clipping;
 };
 
@@ -245,7 +251,7 @@ struct PointCloud {
 
   out_position = drw_point_world_to_homogenous(pt.P);
 
-  if (point_cloud.use_clipping) {
+  if (point_cloud.use_clipping) [[static_branch]] {
     view_clipping_distances(pt.P);
   }
 
